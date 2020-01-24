2e96de6805003a16bee5f529a6f8516da7fbfb50
xedom <xedom08@gmail.com>
2020-01-24 22:40:00 +0100

    parsed the timer

--- components/Timer.js
+++ components/Timer.js
@@ -1,13 +1,13 @@
 import React, { Component, useState } from 'react';
 import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
-
+import parseTime from '../utils/parseTime'
 
 class Timer extends Component {
   constructor(promps) {
     super(promps)
 
     this.state = {
-      counter: 0,
+      counter: 300,
       running: false,
       step: null,
     }
@@ -19,14 +19,14 @@ class Timer extends Component {
       clearInterval(this.state.step)
       this.setState({
         running: false,
-        counter: 0,
+        counter: 300,
         step: null
       })
     } else {
       this.setState({
         running: true,
         step: setInterval(() => {
-          this.setState(prev => ({counter: prev.counter + 1}))
+          this.setState(prev => ({counter: prev.counter - 1}))
         }, 1000)
       })
     }
@@ -35,7 +35,7 @@ class Timer extends Component {
   render() {
     return (
       <TouchableHighlight style={styles.time} onPress={this.onClick.bind(this)}>
-        <Text style={styles.counter}>{this.state.counter}</Text>
+        <Text style={styles.counter}>{parseTime(this.state.counter)}</Text>
       </TouchableHighlight>
     )
   }
